b57cf91ed5cd0be884f1cd686eb798958eaa1daa
cout970 <thecout970@gmail.com>
2014-12-29 22:30:07 +0100

    derp Signed-off-by: cout970 <thecout970@gmail.com>

--- src/com/cout970/worldeditor/RenderUtil.java
+++ src/com/cout970/worldeditor/RenderUtil.java
@@ -9,36 +9,36 @@ public class RenderUtil {
 	public static void block() {
 		
 		//+y
-//		glTexCoord2f(0, 0);
-//		glVertex3f(0,0,0);
-//		glTexCoord2f(1, 0);
-//		glVertex3f(0,0,1);
-//		glTexCoord2f(1, 1);
-//		glVertex3f(1,0,1);
-//		glTexCoord2f(0, 1);
-//		glVertex3f(1,0,0);
+		glTexCoord2f(0, 0);
+		glVertex3f(0,0,0);
+		glTexCoord2f(1, 0);
+		glVertex3f(0,0,1);
+		glTexCoord2f(1, 1);
+		glVertex3f(1,0,1);
+		glTexCoord2f(0, 1);
+		glVertex3f(1,0,0);
 		
 		//-y
-//		glTexCoord2f(0, 0);
-//		glVertex3f(0,1,0);
-//		glTexCoord2f(1, 0);
-//		glVertex3f(0,1,1);
-//		glTexCoord2f(1, 1);
-//		glVertex3f(1,1,1);
-//		glTexCoord2f(0, 1);
-//		glVertex3f(1,1,0);
+		glTexCoord2f(0, 0);
+		glVertex3f(0,1,0);
+		glTexCoord2f(1, 0);
+		glVertex3f(0,1,1);
+		glTexCoord2f(1, 1);
+		glVertex3f(1,1,1);
+		glTexCoord2f(0, 1);
+		glVertex3f(1,1,0);
 		
-//		//+z
-//		glTexCoord2f(0, 0);
-//		glVertex3f(0,0,0);
-//		glTexCoord2f(1, 0);
-//		glVertex3f(0,1,0);
-//		glTexCoord2f(1, 1);
-//		glVertex3f(1,1,0);
-//		glTexCoord2f(0, 1);
-//		glVertex3f(1,0,0);
+		//+z
+		glTexCoord2f(0, 0);
+		glVertex3f(0,0,0);
+		glTexCoord2f(1, 0);
+		glVertex3f(0,1,0);
+		glTexCoord2f(1, 1);
+		glVertex3f(1,1,0);
+		glTexCoord2f(0, 1);
+		glVertex3f(1,0,0);
 		
-//		//-z
+		//-z
 		glTexCoord2f(0, 0);
 		glVertex3f(0,0,1);
 		glTexCoord2f(1, 0);
@@ -58,15 +58,15 @@ public class RenderUtil {
 		glTexCoord2f(0, 1);
 		glVertex3f(0,1,0);
 		
-//		//-x
-//		glTexCoord2f(0, 0);
-//		glVertex3f(1,0,0);
-//		glTexCoord2f(1, 0);
-//		glVertex3f(1,0,1);
-//		glTexCoord2f(1, 1);
-//		glVertex3f(1,1,1);
-//		glTexCoord2f(0, 1);
-//		glVertex3f(1,1,0);
+		//-x
+		glTexCoord2f(0, 0);
+		glVertex3f(1,0,0);
+		glTexCoord2f(1, 0);
+		glVertex3f(1,0,1);
+		glTexCoord2f(1, 1);
+		glVertex3f(1,1,1);
+		glTexCoord2f(0, 1);
+		glVertex3f(1,1,0);
 	}
 
 	public static void bindTexture(Texture texture) {
